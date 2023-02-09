613e47cc6dcfba7f9395f776efa9eda05f451f8c
Kiru <Kiruthegreat2@gmail.com>
2023-02-09 12:37:12 -0600

    comments for last commit

--- initDatabase.h
+++ initDatabase.h
@@ -14,7 +14,7 @@ public:
         QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
         db.setHostName("kiru");
         db.setDatabaseName("resturantDB");
-        db.setUserName("kiru"); // creating database
+        db.setUserName("kiru"); // Database settings
         db.setPassword("kiru");
         bool isDbOk = db.open();
         if (!isDbOk) {
@@ -22,20 +22,21 @@ public:
         }
 
         QSqlQuery q;
-        if (!q.exec(FOOD_SQL))
-            qInfo() << q.lastError();
+        if (!q.exec(FOOD_SQL)) // create table if not exist
+            qInfo() << q.lastError(); // just ingore the error if it exists
     }
 
+    // When adding strings to this method use QLatin1String("string example)
     void addFood (const QString &name, double price, const QString &ingredients ) {
         QSqlQuery q;
-        if (!q.prepare(INSERT_FOOD_SQL)) {
+        if (!q.prepare(INSERT_FOOD_SQL)) { // creates new row and null entries under specified columns (name, price, ingredients)
             qCritical() << q.lastError();
         } else {
-            q.addBindValue(name);
-            q.addBindValue(price);
-            q.addBindValue(ingredients);
+            q.addBindValue(name); // fills null entry for name column
+            q.addBindValue(price); // fills null entry for price column
+            q.addBindValue(ingredients); // fills null entry for ingredients column
 
-            q.exec();
+            q.exec(); // run sql code
         }
     }
 };
