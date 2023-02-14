f9b3765c2328e6a1142fc87fc3d283a00c58af58
Aditya Naidu <adityanaidu16344@gmail.com>
2023-02-14 12:15:41 -0600

    ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,59 @@
 #include "mainwindow.h"
+#include <iostream>
 #include "./ui_mainwindow.h"
 
+
+bool adminMode = false;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
+    setWindowTitle("Drag and drop from QListWidget v1");
+    resize(350,250);
+
+
     delete ui;
 }
+
+void MainWindow::on_appetizers_btn_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_drinks_btn_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_entrees_btn_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_desserts_btn_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_adminButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_adminButton_toggled(bool checked)
+{
+    if(checked) {
+        adminMode = true;
+    }
+    else {
+        adminMode = false;
+    }
+}
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidget>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QHBoxLayout>
+#include "customrectitem.h"
+#include <QKeyEvent>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +22,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_appetizers_btn_clicked();
+
+    void on_desserts_btn_clicked();
+
+    void on_drinks_btn_clicked();
+
+    void on_entrees_btn_clicked();
+
+    void on_adminButton_clicked();
+
+    void on_adminButton_toggled(bool checked);
+
 private:
     Ui::MainWindow *ui;
+
+
 };
 #endif // MAINWINDOW_H
